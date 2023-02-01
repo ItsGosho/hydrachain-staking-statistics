7e12fc4c95dc62d81a73e0eb500e52e7057d144b
itsgosho <itsgosho2@gmail.com>
2023-02-01 14:25:02 +0200

    Wrapping reading from pandas to a tranctions class

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 import pandas
 
+class Transaction:
+    def __init__(self, confirmed, date, type, label, address, amount, id):
+        self.confirmed = confirmed
+        self.date = date
+        self.type = type
+        self.label = label
+        self.address = address
+        self.amount = amount
+        self.id = id
+
 test = pandas.read_csv(r'C:\Users\itsgo\Desktop\hydra-export-1.csv')
 
 for index, row in test.iterrows():
-    isConfirmed = row['Confirmed']
+    confirmed = row['Confirmed']
     date = row['Date']
     type = row['Type']
     label = row['Label']
@@ -11,12 +21,14 @@ for index, row in test.iterrows():
     amount = row['Amount (HYDRA)']
     ID = row['ID']
 
-    print(str(isConfirmed) + ' ' +
-              str(date) + ' ' +
-                  str(type) + ' ' +
-                      str(label) + ' ' +
-                          str(address) + ' ' +
-                              str(amount) + ' ' +
-                                  str(ID))
+    transaction = Transaction(confirmed, date, type, label, address, amount,  ID)
+
+    print(str(transaction.confirmed) + ' ' +
+              str(transaction.date) + ' ' +
+                  str(transaction.type) + ' ' +
+                      str(transaction.label) + ' ' +
+                          str(transaction.address) + ' ' +
+                              str(transaction.amount) + ' ' +
+                                  str(transaction.id))
     print('-------')
 
