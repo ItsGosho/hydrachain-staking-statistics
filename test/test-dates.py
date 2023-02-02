45ca379656a81a30913ac5ec835fc9b6538113de
itsgosho <itsgosho2@gmail.com>
2023-02-02 14:32:57 +0200

    The fetching of last month days will return a date objects instead of just last days

--- test/test-dates.py
+++ test/test-dates.py
@@ -1,12 +1,17 @@
 import calendar
 from datetime import datetime
 
+# class LastMonthDay:
+#     def __init__(self, year, month, day):
+#         self.year = year
+#         self.month
+
 """
 Collect the last day of each month between two months in a given year
 Example:
 From year 2022 and month 11 (November) to year 2023 and month 2 (February)
 Result:
-[31, TODO:]
+[datetime.date(2022, 11, 30), datetime.date(2022, 12, 31), datetime.date(2023, 1, 31), datetime.date(2023, 2, 28)]
 """
 def collectLastMonthDays(fromYear, fromMonth, toYear, toMonth):
     lastMonthDays = []
@@ -19,7 +24,8 @@ def collectLastMonthDays(fromYear, fromMonth, toYear, toMonth):
             return lastMonthDays
 
         tuple = calendar.monthrange(fromYear, fromMonth)
-        lastMonthDays.append(tuple[1])
+        date = datetime(fromYear, fromMonth, tuple[1]).date()
+        lastMonthDays.append(date)
 
         fromMonth += 1
 
