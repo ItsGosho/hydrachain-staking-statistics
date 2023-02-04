e13e8c674f3737c1215985c43cdb40af1b24a476
itsgosho <itsgosho2@gmail.com>
2023-02-04 20:05:15 +0200

    Implemented the hydra prices into the core

--- hydrachain-staking-statistics.py
+++ hydrachain-staking-statistics.py
@@ -1,32 +1,14 @@
 import re
 import json
 import hydra_transactions_reader
+import hydra_prices
 
 transactions = hydra_transactions_reader.readTransactions(r'C:\Users\itsgo\Desktop\hydra-export-1.csv')
 usdToBGNRate = float(1.80)
 hydraPriceTodayUSD = float(2.00)
 byMonth = {}
-prices = {} #By month at the end of the month in usd
-prices['1/2023'] = float(2.00)
-prices['12/2022'] = float(1.48)
-prices['11/2022'] = float(1.60)
-prices['10/2022'] = float(2.62)
-prices['9/2022'] = float(2.09)
-prices['8/2022'] = float(2.74)
-prices['7/2022'] = float(2.98)
-prices['6/2022'] = float(2.44)
-prices['5/2022'] = float(3.85)
-prices['4/2022'] = float(6.88)
-prices['3/2022'] = float(8.76)
-prices['2/2022'] = float(7.27)
-prices['1/2022'] = float(8.25)
-prices['12/2021'] = float(8.01)
-prices['11/2021'] = float(13.25)
-prices['10/2021'] = float(17.24)
-prices['9/2021'] = float(18.04)
-prices['8/2021'] = float(18.18)
-prices['7/2021'] = float(16.80)
-prices['6/2021'] = float(28.42)
+hydra_prices.synchronizeAllMonthsPricesForLastMonthDay()
+prices = hydra_prices.getAllLastDayOfMonthPricesFormatted()
 
 for transaction in transactions:
 
