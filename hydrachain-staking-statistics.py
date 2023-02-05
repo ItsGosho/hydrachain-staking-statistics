512e144f95341413e678f906ed9864e9d488b46d
itsgosho <itsgosho2@gmail.com>
2023-02-05 17:34:57 +0200

    Replaced the JSON Output with Tables

--- hydrachain-staking-statistics.py
+++ hydrachain-staking-statistics.py
@@ -2,14 +2,19 @@ import re
 import json
 import hydra_transactions_reader
 import hydra_prices
+from prettytable import PrettyTable as pt
 
 transactions = hydra_transactions_reader.readTransactions(r'C:\Users\itsgo\Desktop\hydra-export-1.csv')
-usdToBGNRate = float(1.80) #TODO:
+usdToBGNRate = float(1.80)  # TODO:
 hydraPriceTodayUSD = hydra_prices.getCurrentHydraPrice()
 hydra_prices.synchronizeAllMonthsPricesForLastMonthDay()
 prices = hydra_prices.getAllLastDayOfMonthPricesFormatted()
 
-byMonth = {}
+byMonths = {}
+tableMontlyStakingStatistics = pt(title="Monthly Staking Statistics")
+tableMontlyStakingStatistics.field_names = ["Month", "Mined", "Transactions", "Lowest Block", "Highest Block", "Month End USD", "Month End BGN",
+                  "Today USD", "Today BGN"]
+tableMontlyStakingStatistics.align = "r"
 
 for transaction in transactions:
 
@@ -19,7 +24,7 @@ for transaction in transactions:
     minedMonthYear = str(transaction.date.month) + '/' + str(transaction.date.year)
     minedAmount = float(transaction.amount)
 
-    if minedMonthYear not in byMonth:
+    if minedMonthYear not in byMonths:
         obj = {"total": minedAmount, "transactions": int(1), "blockMin": minedAmount, "blockMax": minedAmount,
                "usdEndMonth": float(0), "bgnEndMonth": float(0), "usdEquivalentToday": float(0),
                "bgnEquivalentToday": float(0)}
@@ -27,10 +32,10 @@ for transaction in transactions:
         obj["bgnEndMonth"] = obj["usdEndMonth"] * usdToBGNRate
         obj["usdEquivalentToday"] = prices[minedMonthYear] * hydraPriceTodayUSD
         obj["bgnEquivalentToday"] = obj["usdEquivalentToday"] * usdToBGNRate
-        byMonth[minedMonthYear] = obj
+        byMonths[minedMonthYear] = obj
         continue
-    if minedMonthYear in byMonth:
-        obj = byMonth[minedMonthYear]
+    if minedMonthYear in byMonths:
+        obj = byMonths[minedMonthYear]
         obj["total"] = obj["total"] + minedAmount
         obj["transactions"] = obj["transactions"] + 1
         obj["blockMin"] = minedAmount if minedAmount < obj["blockMin"] else obj["blockMin"]
@@ -39,21 +44,38 @@ for transaction in transactions:
         obj["bgnEndMonth"] = obj["usdEndMonth"] * usdToBGNRate
         obj["usdEquivalentToday"] = obj["total"] * hydraPriceTodayUSD
         obj["bgnEquivalentToday"] = obj["usdEquivalentToday"] * usdToBGNRate
-        byMonth[minedMonthYear] = obj
+        byMonths[minedMonthYear] = obj
 
-print('Statistics by month:')
-print(json.dumps(byMonth, indent=4))
+for byMonth in byMonths:
+    tableMontlyStakingStatistics.add_row([
+        byMonth,
+        round(byMonths[byMonth]["total"], 2),
+        byMonths[byMonth]["transactions"],
+        round(byMonths[byMonth]["blockMin"], 2),
+        round(byMonths[byMonth]["blockMax"], 2),
+        round(byMonths[byMonth]["usdEndMonth"], 2),
+        round(byMonths[byMonth]["bgnEndMonth"], 2),
+        round(byMonths[byMonth]["usdEquivalentToday"], 2),
+        round(byMonths[byMonth]["bgnEquivalentToday"], 2),
+    ])
+
+print(tableMontlyStakingStatistics)
 
 totalMined = float(0)
 totalTransactions = int(0)
 
-for byMonthStatistic in byMonth:
-    totalMined += byMonth[byMonthStatistic]["total"]
-    totalTransactions += byMonth[byMonthStatistic]["transactions"]
+for byMonthStatistic in byMonths:
+    totalMined += byMonths[byMonthStatistic]["total"]
+    totalTransactions += byMonths[byMonthStatistic]["transactions"]
 
-print("")
-print("Statistics overall:")
-print("Mined: " + str(totalMined))
-print("Transactions: " + str(totalTransactions))
-print("USD Equivalent Today:" + str((totalMined * hydraPriceTodayUSD)))
-print("BGN Equivalent Today:" + str((totalMined * hydraPriceTodayUSD) * usdToBGNRate))
+tableOverallStakingStatistics = pt(title="Overall Staking Statistics")
+tableOverallStakingStatistics.field_names = ["Mined", "Transactions", "Today USD", "Today BGN"]
+tableOverallStakingStatistics.align = "r"
+tableOverallStakingStatistics.add_row([
+    str(totalMined),
+    str(totalTransactions),
+    str((totalMined * hydraPriceTodayUSD)),
+    str((totalMined * hydraPriceTodayUSD) * usdToBGNRate)
+])
+
+print(tableOverallStakingStatistics)
